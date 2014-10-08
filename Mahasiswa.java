32983439de17866acd9a678e7e5fe2527ddcea21
arminmaulana <arminmaulana@gmail.com>
2014-10-08 20:59:01 +0700

    a

---
+++ Mahasiswa.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+public class Mahasiswa{
+	int nilai;
+	public float nilai;
+	public static void main(String[]args){
+	Scanner masuk = new Scanner (System.in);
+		Mahasiswa orang= new Mahasiswa();
+		orang.setNilai("98");
+		System.out.println ("Nilaiku adalah =" + orang.getNilai());
+	}
+	public void setNama (String namaku){
+		this.nilai=nilaiku;
+		}
+		public String getNama() {
+			return this.nilai;
+		}
+	}
